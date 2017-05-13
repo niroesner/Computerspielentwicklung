bd27f2e0653f1b181b029de5b8f0b35be1a76e66
Sylwia Miśkowiec <sylwia.miskowiec@hatimeria.pl>
2017-05-13 21:49:55 +0200

    add jumping in different directions for desktop

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+	private CharacterController controller;
+
+	private float verticalVelocity;
+	private float horizontalVelocity;
+	private float gravity = 14.0f;
+	private float jumpForce = 8.0f;
+
+
+	private void Start () {
+		controller = GetComponent<CharacterController> (); 
+	}
+
+	private void Update () {
+		if (controller.isGrounded) {
+			verticalVelocity = -gravity * Time.deltaTime;
+
+			if (Input.GetKeyDown (KeyCode.Space) || Input.touchCount > 0) {
+				verticalVelocity = jumpForce;
+				horizontalVelocity = jumpForce * 2.0f;
+			}
+		} else {
+			verticalVelocity -= gravity * Time.deltaTime;
+			horizontalVelocity = 0;
+		}
+
+		Vector3 moveVector = new Vector3 (horizontalVelocity, verticalVelocity, 0);
+		controller.Move (moveVector * Time.deltaTime);
+	}
+}
