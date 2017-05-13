19d0e6b002731e863dcd2f88d2b922b86a14d2b6
Sylwia Miśkowiec <sylwia.miskowiec@hatimeria.pl>
2017-05-13 21:58:59 +0200

    add project files

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,29 +7,49 @@ public class PlayerMotor : MonoBehaviour {
 	private CharacterController controller;
 
 	private float verticalVelocity;
-	private float horizontalVelocity;
 	private float gravity = 14.0f;
 	private float jumpForce = 8.0f;
-
+	private float horizontalMove = 0;
+	private float verticalMove = 0;
 
 	private void Start () {
 		controller = GetComponent<CharacterController> (); 
 	}
 
 	private void Update () {
+		Vector3 moveVector = Vector3.zero;
+		float xValue = 0;
+		float zValue = 0;
+	
 		if (controller.isGrounded) {
 			verticalVelocity = -gravity * Time.deltaTime;
-
-			if (Input.GetKeyDown (KeyCode.Space) || Input.touchCount > 0) {
+			if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0 || Input.touchCount > 0) {
 				verticalVelocity = jumpForce;
-				horizontalVelocity = jumpForce * 2.0f;
+				horizontalMove = 0;
+				verticalMove = 0;
+
+				Debug.Log (Input.GetTouch (0).deltaPosition);
+
+				if (Input.GetAxis ("Horizontal") > 0) {
+					horizontalMove = 1;
+				} else if (Input.GetAxis ("Horizontal") < 0) {
+					horizontalMove = -1;
+				} else if (Input.GetAxis ("Vertical") > 0) {
+					verticalMove = 1;
+				} else if (Input.GetAxis ("Vertical") < 1) {
+					verticalMove = -1;
+				}
 			}
 		} else {
+			xValue = horizontalMove;
+			zValue = verticalMove;
 			verticalVelocity -= gravity * Time.deltaTime;
-			horizontalVelocity = 0;
 		}
 
-		Vector3 moveVector = new Vector3 (horizontalVelocity, verticalVelocity, 0);
+		moveVector.x = xValue;
+		moveVector.y = verticalVelocity;
+		moveVector.z = zValue;
+
 		controller.Move (moveVector * Time.deltaTime);
 	}
 }
